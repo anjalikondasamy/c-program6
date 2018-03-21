3a562891353b50966b6cb99c18ae9f338bc0cfec
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-21 16:39:33 +0530

    checkprime.c

---
+++ checkprime.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,i,count=0;
+  clrscr();
+  printf("Enter the number");
+  scanf("%d",&n);
+	for(i=2;i<=n;i++)
+	{
+		if(n%i==0)
+		count++;
+	}
+	if(count<3)
+	{
+		printf("prime number");
+	}
+	else
+	{
+		printf("not a prime number");
+	}
+	return 0;
+}
